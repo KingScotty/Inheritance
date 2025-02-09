fdc3e36385ddf5edf8d8148c678ab75d0375da14
Scott Browning <scottybrowning12345@gmail.com>
2025-02-09 16:32:15 -0500

    Expanded Salary.java and created test.

--- src/Salary.java
+++ src/Salary.java
@@ -1,2 +1,50 @@
-public class Salary {
+public class Salary extends Worker {
+    private double annualSalary;
+
+    public Salary(String firstName, String lastName, String ID, String title, int YOB, double annualSalary) {
+        super(firstName, lastName, ID, title, YOB, annualSalary / 52 / 40);
+        this.annualSalary = annualSalary;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double calculatePay(double hoursWorked) {
+        return annualSalary / 52;
+    }
+
+    @Override
+    public void displayWeeklyPay(double hoursWorked) {
+        System.out.println("Worker: " + getFirstName() + " " + getLastName());
+        System.out.println("Weekly Salary: $" + calculatePay(hoursWorked));
+        System.out.println("-------------------------");
+    }
+
+    @Override
+    public String toString() {
+        return "Salary{" +
+                "annualSalary=" + annualSalary +
+                '}';
+    }
+
+    @Override
+    public String toCSV() {
+        return super.toCSV() + "," + annualSalary;
+    }
+
+    @Override
+    public String toXML() {
+        return super.toXML() + "<annualSalary>" + annualSalary + "</annualSalary>\n";
+    }
+
+    @Override
+    public String toJSON() {
+        return super.toJSON().replace("}", ", \"annualSalary\": " + annualSalary + "}");
+    }
 }
---
+++ src/SalaryTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryTest {
+
+    @Test
+    void getAnnualSalary() {
+    }
+
+    @Test
+    void setAnnualSalary() {
+    }
+
+    @Test
+    void calculatePay() {
+    }
+
+    @Test
+    void displayWeeklyPay() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void toCSV() {
+    }
+
+    @Test
+    void toXML() {
+    }
+
+    @Test
+    void toJSON() {
+    }
+}
